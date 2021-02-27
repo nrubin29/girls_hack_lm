277cbc0ff9b91399bfcb9f32bdbb8693798e00e3
nrubin29 <nrubin29@gmail.com>
2021-02-27 16:21:49 -0500

    Added support for static and media files when running locally

--- girls_hack_lm/urls.py
+++ girls_hack_lm/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import os
+
+from girls_hack_lm import settings
 
 urlpatterns = [
     path('', include('hackathon.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if os.getenv('LOCAL'):
+    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
